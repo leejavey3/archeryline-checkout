fe011ffd054c7823b7ca0b492876e89aa99d7c7e
leejavey3 <leejavey@gmail.com>
2026-06-07 11:23:04 +0800

    Use Archeryline branding on checkout errors

--- components/composite/ErrorContainer/index.tsx
+++ components/composite/ErrorContainer/index.tsx
@@ -1,7 +1,7 @@
 import { Base } from "components/ui/Base"
 import { Container } from "components/ui/Container"
 import { Footer } from "components/ui/Footer"
-import { Logo } from "components/ui/Footer/cl"
+import { Logo } from "components/ui/Logo"
 
 export const ErrorContainer = ({ children }: { children: ChildrenType }) => {
   return (
@@ -10,7 +10,7 @@ export const ErrorContainer = ({ children }: { children: ChildrenType }) => {
         <div className="flex flex-wrap justify-end items-stretch flex-col h-screen p-5 md:p-10 lg:px-20 lg:pb-10">
           <div className="md:max-w-xs">
             <div className="self-center text-black md:pl-4 md:self-auto">
-              <Logo />
+              <Logo logoUrl={null} companyName="Archeryline" />
             </div>
           </div>
           <div className="flex flex-col flex-1 justify-center items-center text-center">
